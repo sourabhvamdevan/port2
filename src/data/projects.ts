087fd0725a53b1631b18b84168227fe90466180b
Sourabh Vamdevan <vamdevansourabh@gmail.com>
2026-01-30 10:15:49 +0530

    done changes in projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -42,6 +42,7 @@ export const projects: Project[] = [
     description: "Created a fullstack website using NextJS to track sleep patterns.",
     tags: ["NextJS", "React19","Typescript", "Prisma", "Material-UI", "Clerk Authentication","ChartJS","Neon"],
     github: "https://github.com/sourabhvamdevan/nextjs-sleep_tracker",
+    image:"https://www.shutterstock.com/image-photo/closeup-hand-adjusting-alarm-clock-260nw-2616272297.jpg",
   },
  
    {
@@ -50,6 +51,7 @@ export const projects: Project[] = [
     description: "Created AI based doubt solver using python and streamlit.",
     tags: ["Python", "Python Virtual Environment", "Streamlit"],
     github: "https://github.com/sourabhvamdevan/doubt-ai",
+    image:"https://www.shutterstock.com/image-photo/yellow-sticky-note-black-lettering-600nw-2545747269.jpg",
   },
 
 
@@ -59,6 +61,7 @@ export const projects: Project[] = [
     description: "Created AI based doubt solver using python and streamlit.",
     tags: ["Flutter", "Dart", "Material-UI-3", "Riverpod"],
     github: "https://github.com/sourabhvamdevan/expense_demo",
+    image:"https://www.shutterstock.com/image-photo/computer-app-money-budget-expense-600nw-2001343229.jpg",
   },
 
    {
@@ -67,6 +70,7 @@ export const projects: Project[] = [
     description: "Created AI based doubt solver using python and streamlit.",
     tags: ["NextJS", "React19", "TailwindCSS", "Clerk", "flowbite UI"],
     github: "https://github.com/sourabhvamdevan/nextjs-blog-demo",
+    image:"https://www.shutterstock.com/image-vector/blog-articles-creation-concept-flat-600nw-2408164525.jpg",
   },
 
    {
@@ -75,6 +79,7 @@ export const projects: Project[] = [
     description: "Created AI based flutter app to help students in their studies.",
     tags: ["Flutter", "GeminiAPI", "Hive",  "Material-UI-3","(BERT, BART, GPT-4)"],
     github: "https://github.com/sourabhvamdevan/ai_study",
+    image:"https://www.shutterstock.com/image-photo/primary-school-boy-working-on-600nw-2674058367.jpg",
   },
 
    {
@@ -83,6 +88,7 @@ export const projects: Project[] = [
     description: "Created travel app to help users remember places they visited.",
     tags: ["Flutter", "Firebase", "Material-UI-3","Riverpod"],
     github: "https://github.com/sourabhvamdevan/travel_app_assignment",
+    image:"https://www.shutterstock.com/image-photo/traveler-using-smartphone-laptop-booking-600nw-2591808895.jpg",
   },
 
    {
@@ -91,6 +97,7 @@ export const projects: Project[] = [
     description:"The app allows users to schedule medicine reminders and receive notifications at the correct time.",
     tags: ["Flutter", "Hive", "Material-UI-3","Flutter Local Notifications"],
     github: "https://github.com/sourabhvamdevan/medical_app_assignment",
+    image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRa59VDaqu5PdqmmN6Mcsex-tPiDPq3W_VXxA&s",
   },
 
 
@@ -100,6 +107,7 @@ export const projects: Project[] = [
     description:"This app generates personalized course based on user input using Gemini API.",
     tags: ["Flutter", "Dart", "Material-UI-3","Flask", "Python Virtual Environment","Gemini API"],
     github: "https://github.com/sourabhvamdevan/ai_course_generator",
+    image:"https://img.freepik.com/free-vector/landing-page-artificial-intelligence-template_23-2148376199.jpg?semt=ais_hybrid&w=740&q=80",
   },
 
 
