6c1f54996b0a0c079882055122e6868602e30967
Sourabh Vamdevan <vamdevansourabh@gmail.com>
2026-01-30 12:23:06 +0530

    added images

--- src/data/certifications.ts
+++ src/data/certifications.ts
@@ -13,60 +13,75 @@ export const certifications = [
     issuer: "Stanford Online",
     year: "2025",
     link: "https://drive.google.com/file/d/1HPya9xs2enuny7pvZrx2H-TYxUMKKZNs/view?usp=sharing",
+    image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTB92t-iq0LO1z33kAHu9U8fqaIRza0a90L-g&s",
   },
   {
     title: "Problem Solving – Intermediate",
     issuer: "HackerRank",
     year: "2024",
     link: "https://drive.google.com/file/d/15zrAobpIhwHInobSjwCORJQGJR4uVB40/view?usp=sharing",
+    image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQW088VU0KlTZblYg8FzGVk8wT8yskjLr1kRg&s",
   },
    {
     title: "Cloud Computing NPTEL",
     issuer: "NPTEL",
     year: "2025",
     link: "https://drive.google.com/file/d/1bAOe_l7rbwocBduqXIPe83pNiuZd8My1/view?usp=sharing",
+    image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_0jAzKx8G5TDgoi4pvT8d42016sVrzmM3Dg&s",
   },
    {
     title: "NPTEL Believer Certificate",
     issuer: "NPTEL",
     year: "2025",
     link: "https://drive.google.com/file/d/1B8r5X-wv6KmIPwIjEt9E4FECtBWDVVlf/view?usp=sharing",
+    image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_0jAzKx8G5TDgoi4pvT8d42016sVrzmM3Dg&s",
   },
    {
     title: "Full Stack Development GeeksForGeeks",
     issuer: "GeeksForGeeks",
     year: "2025",
     link: "https://drive.google.com/file/d/1Sk53C9BzAdMGG-CxBakbLEggqvjQ7W1a/view?usp=drivesdk",
+    image:"https://media.geeksforgeeks.org/wp-content/cdn-uploads/20210419113249/gfg-new-logo-min.png",
+
   },
   {
     title: "Databases with SQL Harvard University",
     issuer: "CS-50 Harvard University",
     year: "2024",
     link: "https://drive.google.com/file/d/18VVYZZSZR3KiFKl4B20PGkIiF7iUp4nR/view?usp=sharing",
+    image:"https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Harvard_University_logo.svg/960px-Harvard_University_logo.svg.png",
   },
     {
     title: "Front End Developer Skill Certificate-HackerRank",
     issuer: "HackerRank",
     year: "2025",
     link: "https://drive.google.com/file/d/12PCKQWooX1UE99XdXH7LI2AAu2So3ekU/view?usp=sharing",
+    image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQW088VU0KlTZblYg8FzGVk8wT8yskjLr1kRg&s",
+
   },
     {
     title: "SQL Intermediate Skill Certificate-HackerRank",
     issuer: "HackerRank",
     year: "2025",
     link: "https://drive.google.com/file/d/1xxA_2rVdSF3HD0DzOVjEZ_7Hxp9xXvKB/view?usp=sharing",
+    image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQW088VU0KlTZblYg8FzGVk8wT8yskjLr1kRg&s",
+
   },
    {
     title: "Software Engineering skill certificate-HackerRank",
     issuer: "HackerRank",
     year: "2025",
     link: "https://drive.google.com/file/d/1G_zAZIhrzl1OtIBJnO6UM_IA4LIac7Df/view?usp=sharing",
+    image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQW088VU0KlTZblYg8FzGVk8wT8yskjLr1kRg&s",
+
   },
    {
     title: "Data Structures and Algorithms using Java",
     issuer: "NPTEL",
     year: "2025",
     link: "https://drive.google.com/file/d/1e_TBDe7ha4f9vTadb8xVvcGK8FIJV0vE/view?usp=sharing",
+    image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_0jAzKx8G5TDgoi4pvT8d42016sVrzmM3Dg&s",
+
   },
 
    {
@@ -74,12 +89,16 @@ export const certifications = [
     issuer: "GeeksForGeeks",
     year: "2025",
     link: "https://drive.google.com/file/d/1BdyJJxREgPc_SIWWOWQuXNjC7I0kobbH/view?usp=sharing",
+    image:"https://media.geeksforgeeks.org/wp-content/cdn-uploads/20210419113249/gfg-new-logo-min.png",
+
   },
    {
     title: "Machine Learning Specialization Stanford University",
     issuer: "Stanford Online",
     year: "2024",
     link: "https://drive.google.com/file/d/1K4Zlw843E8iQG2XtrD4MofXKKerXRxs2/view?usp=sharing",
+    image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTB92t-iq0LO1z33kAHu9U8fqaIRza0a90L-g&s",
+
   },
 
    {
@@ -87,6 +106,8 @@ export const certifications = [
     issuer: "IBM",
     year: "2025",
     link: "https://drive.google.com/file/d/1GPFxVAA8eoM3qumt3A2wqON41TLuZZm2/view?usp=sharing",
+    image:"https://substackcdn.com/image/fetch/$s_!aFzv!,f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fsubstack-post-media.s3.amazonaws.com%2Fpublic%2Fimages%2Fa27a43b7-f1ec-4586-9898-5f43c9b7cdbf_1000x1000.jpeg",
+
   },
 
    {
@@ -94,6 +115,8 @@ export const certifications = [
     issuer: "Harvard University",
     year: "2024",
     link: "https://drive.google.com/file/d/1d1LpQubQBBgtWb1yL7sSvO2ZjnKMuX0P/view?usp=sharing",
+    image:"https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Harvard_University_logo.svg/960px-Harvard_University_logo.svg.png",
+
   },
 
 
